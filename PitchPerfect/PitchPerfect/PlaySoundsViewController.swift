cb7b8060c9ea47527a15ebd2c01adaabe72e079e
Noah J Anderson <noah.anderson.48@my.csun.edu>
2015-08-02 04:54:50 -0700

    Completed PlaySoundsAudioController main features. Need to add a pause and play button and implementation.

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -16,11 +16,15 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var lowPitchButton: UIButton!
     var audioPlayer: AVAudioPlayer!
     var receivedAudio: RecordedAudio!
+    var audioEngine: AVAudioEngine!
+    var audioFile: AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,9 +44,11 @@ class PlaySoundsViewController: UIViewController {
         }
         else if sender == highPitchButton{
             println("High Pitch Button")
+            playAudioAtPitch(1000.0)
         }
         else if sender == lowPitchButton{
             println("Low Pitch Button")
+            playAudioAtPitch(-500.0)
         }
     }
     
@@ -51,6 +57,22 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.currentTime = 0.0
         audioPlayer.rate = rate
         audioPlayer.play()
+    }
+    
+    func playAudioAtPitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        var audioPlayerNode: AVAudioPlayerNode! = AVAudioPlayerNode()
+        var audioPitchNode: AVAudioUnitTimePitch! = AVAudioUnitTimePitch()
+        audioPitchNode.pitch = pitch
+        audioEngine.attachNode(audioPlayerNode)
+        audioEngine.attachNode(audioPitchNode)
+        audioEngine.connect(audioPlayerNode, to: audioPitchNode, format: nil)
+        audioEngine.connect(audioPitchNode, to: audioEngine.outputNode, format: nil)
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        audioPlayerNode.play()
     }
 
     /*
